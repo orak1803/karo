8fedec9ef9831534eeabbe179044cd6a9a6f9a2b
michane <michane@ac.sce.ac.il>
2018-06-08 14:53:47 +0300

    k

--- js/initService.js
+++ js/initService.js
@@ -1,34 +1,72 @@
 
 var leftContainer,
-inputList;
+foodList,
+inputList = [],
+inputSteps,
+calcBtn,
+rightContainer;
 
 
 function createFoodPicker(){
     var container = document.createElement('div');
-    inputList = document.createElement('ul');
+    foodList = document.createElement('ul');
+    foodList.classList.add('foodList');
     veganFoodService.getFoodTypes().forEach(fillList);
     meatFoodService.getFoodTypes().forEach(fillList);
-    container.appendChild(inputList);
+    container.appendChild(foodList);
     return container;
 }
 
 function fillList(element){
     var item = document.createElement('li');
+    item.classList.add('item');
     var itemTitle = document.createElement('span');
     itemTitle.classList.add('itemTitle');
-    var itemInput = document.createElement('input');
-    itemInput.classList.add('itemInput');
+    inputList.push(document.createElement('input'));
+    var index = inputList.length - 1;
+    inputList[index].classList.add('itemInput');
     itemTitle.innerHTML = element;
-    itemInput.type = "text";
+    inputList[index].type = "text";
     item.appendChild(itemTitle);
-    item.appendChild(itemInput);
-    inputList.appendChild(item);
+    item.appendChild(inputList[index]);
+    foodList.appendChild(item);
+}
+
+function createFootStepsPicker(){
+    var container = document.createElement('div');
+    container.style.textAlign = 'center';
+    var label = document.createElement('span');
+    label.id = 'stepsLabel';
+    label.innerHTML = 'Steps';
+
+    inputSteps = document.createElement('input');
+    inputSteps.id = 'stepsInput';
+    inputSteps.type = 'text';
+
+    calcBtn = document.createElement('button');
+    calcBtn.classList.add('button');
+    calcBtn.classList.add('b1');
+    calcBtn.id = 'calcBtn';
+    calcBtn.innerHTML = 'compare';
+    calcBtn.addEventListener('click', function (){
+        calcBtn.style.color = 'red';
+    });
+
+    container.appendChild(label);
+    container.appendChild(inputSteps);
+    container.appendChild(calcBtn);
+    return container;
 }
 
 window.onload = function() {
 
     leftContainer = document.getElementById('leftContainer');
     var foodPickerContainer = createFoodPicker();
+
+    rightContainer = document.getElementById('rightContainer');
+    var stepPicker = createFootStepsPicker();
+
     leftContainer.appendChild(foodPickerContainer);
+    rightContainer.appendChild(stepPicker)
 
 }
--- js/veganFoodInfoService.js
+++ js/veganFoodInfoService.js
@@ -8,4 +8,4 @@ var veganFoodService = {};
 
 veganFoodService.getFoodTypes = function() {
     return Object.keys(veganfoodList);
-}
+}   
